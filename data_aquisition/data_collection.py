8f38013d8ed147f056b8087699a29e0ade640edb
Rufael Marew <rufaelfekadu48@gmail.com>
2024-01-01 17:33:50 +0400

    minor updatea

--- data_aquisition/data_collection.py
+++ data_aquisition/data_collection.py
@@ -269,6 +269,8 @@ class Experiment:
             self.emg_data.save_as = str(Path(self.data_dir, f"fpe_pos{self.exp_info['position']}_{self.exp_info['Participant'].rjust(3, '0')}_S{self.exp_info['session']}_rep{self.exp_num}_BT.edf"))
             self.leap_data.save_as = str(Path(self.data_dir, f"fpe_pos{self.exp_info['position']}_{self.exp_info['Participant'].rjust(3, '0')}_S{self.exp_info['session']}_rep{self.exp_num}_BT.csv"))
             print(f"Saving data to: {self.emg_data.save_as}")
+            if self.emg_data.running:
+                self.emg_data.stop()
             self.emg_data.start()
             self.leap_data.start()
 
