dd0794f972146e7b2c75ae9f2a34c70ee836a8ef
Rufael Marew <rufaelfekadu48@gmail.com>
2024-01-18 13:09:57 +0400

    minor updates

--- data_aquisition/Leap/resources/Windows/Leap.py
+++ data_aquisition/Leap/resources/Windows/Leap.py
@@ -1593,5 +1593,5 @@ Listener_swigregister(Listener)
 
 # This file is compatible with both classic and new-style classes.
 if __name__ == "__main__":
-    print(f'finge type:{LeapPython.Finger_TYPE_THUMB}')
+    print(f'finge type:{LeapPython.Finger_TYPE_INDEX}')
 
