8c013ac077a032bf75cbaa9c0a959fe71799e8b2
Rufael Marew <rufaelfekadu48@gmail.com>
2024-01-28 03:08:44 +0400

    minor bug fix

--- src/hpe/trainer/trainer.py
+++ src/hpe/trainer/trainer.py
@@ -39,7 +39,7 @@ class EmgNet(pl.LightningModule):
 
         self.backbone = build_backbone(cfg).to(self.device)
         self.loss_fn = make_loss(cfg)
-
+        print(self.backbone)
         self.criterion = torch.nn.MSELoss()
         self.save_hyperparameters()
 
