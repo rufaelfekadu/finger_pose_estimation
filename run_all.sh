a3d948e24f2cbe18bb489d06f64ef7953498f765
Rufael Marew <rufaelfekadu48@gmail.com>
2024-01-30 02:34:50 +0400

    minor scrip change

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# specify the directory containing the scripts
+dir="./scripts"
+
+# iterate over each .sh file in the directory
+for script in "$dir"/*.sh
+do
+  # if the file is a regular file and is executable
+  if [ -f "$script" ]
+  then
+    # execute the script
+    chmod +x "$script"
+    echo "Executing $script"
+    "$script"
+  fi
+done
