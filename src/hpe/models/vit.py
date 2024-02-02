fb582e1547d4f00d565470b97f19a8596e0dfd65
Rufael Marew <rufaelfekadu48@gmail.com>
2024-02-02 18:46:32 +0400

    increased learning rate

--- src/hpe/models/vit.py
+++ src/hpe/models/vit.py
@@ -173,8 +173,8 @@ def make_vit(cfg):
         depth = 4,
         heads = 4,
         mlp_dim = 2048,
-        dropout = 0.5,
-        emb_dropout = 0.,
+        dropout = 0.25,
+        emb_dropout = 0.5,
         channels=1,
         pool='mean'
     )
