c6a5dbc92821f4e13b6570cb7dc88276897a6f62
Rufael Marew <rufaelfekadu48@gmail.com>
2024-02-04 21:08:28 +0400

    minor update

--- train.sh
+++ train.sh
@@ -14,5 +14,6 @@ log_dir='outputs/'$exp_setup'/'$model'/'$subject'/'$pose
 python main.py --config $config \
     --opts \
     STAGE $stage \
+    DATA.PATH $data_path \
     DATA.EXP_SETUP $exp_setup \
     SOLVER.LOG_DIR $log_dir \
